48b1d15928082198123a0fea1f24e2bc087916a9
dylan <dylan_zeng92@qq.com>
2021-11-26 16:26:55 +0800

    test(core/index): update branch test

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,42 +2,18 @@ import { fetchApiJson, registerTemplates,  writeInterfaces, writeServices, write
 import { writeMustacheFile  } from './utils/files'
 import * as path from 'path'
 import * as fs from 'fs-extra'
+import { SwaggerConfig } from './index.d'
 
-
-interface SwaggerConfig {
-  /**
-   * @description swagger api url
-   */
-  url: string
-  /**
-   * @description single-api or apis
-   */
-  paths?: Array<string|RegExp>
-  /**
-   * @description output floder
-   */
-  output?: string
-  /**
-   * @description where request module import from
-   */
-  requestPath?: string
-  /**
-   * @description request templates only create and remove when it is true
-   * when you only need exportsRequest once, mostly code likes:
-   * * `exportsRequest: !isRequestFloderExsit`
-   */
-  exportsRequest?: boolean
-}
 export default async function main({
   url,
   output,
   paths=undefined,
   requestPath,
-  exportsRequest
+  exportsRequest = true
 }: SwaggerConfig): Promise<void> {
   const templates = registerTemplates()
   const res = await fetchApiJson(fixedEncodeURI(url))
-  const needExports = exportsRequest === undefined ? true : exportsRequest
+  const needExports = exportsRequest
   let folder =  path.join(process.cwd(), output || '/api')
   if (res) {
     const isExists = await fs.pathExistsSync(folder)
