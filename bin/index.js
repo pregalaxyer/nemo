77c131aa533eecc2eefd7e7d9337696d82deb4a8
zjotaro <zzc5464@foxmail.com>
2021-11-29 16:44:37 +0800

    feat: update commonder

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,23 @@
 #!/usr/bin/env node
 process.title = "@pregalaxyer/nemo";
-const commander = require("commander");
+const { program } = require("commander");
 const nemoMail = require("../lib/index");
-// const path = require("path");
-
-commander
-  .version(require("../package").version)
-  .usage("<command> [options]")
-  .command("generate", 'generate file from a template (short-cut alias: "g")');
-
-commander
-  .command("output <api> [output]")
-  .description("output typescript file", {
-    api: "swagger api",
-    output: "typescript file path",
-  })
-  .action((url, output) => {
-    nemoMail({
-      url,
-      output,
-    });
-    console.log("parse swagger api is done!");
+program
+  .allowUnknownOption()
+  .version(require("../package.json").version)
+  .usage("npx @pregalaxyer/nemo mail --url xxx --output xxx");
+// npx @pregalaxyer/nemo mail --url http://10.8.101.10:8010/v2/api-docs --output ./src/api/test
+// !by test ↓
+// node bin/index.js mail --url http://10.8.101.10:8010/v2/api-docs --output ./src/api/test
+program
+  .command("mail")
+  .option("-U, --url [path]", "swagger api url")
+  .option("-O, --output [path]", "output floder")
+  .option("-P, --paths [names]", "single-api or apis")
+  .option("-R, --requestPath [path]", "where request module import from")
+  .option("-E, --exportsRequest", "request templates only create and remove")
+  .description("This is a tiger-cli tool.")
+  .action((options) => {
+    nemoMail(options);
   });
-commander.parse(process.argv);
+program.parse(process.argv);
