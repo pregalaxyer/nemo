9dc438098af461cc44ddaa90366db9daa3e507ff
dylan <dylan_zeng92@163.com>
2022-02-07 18:05:23 +0800

    fix: add support for the json file input (#23)

--- src/core/utils/share.ts
+++ src/core/utils/share.ts
@@ -1,21 +1,28 @@
 
 import fetch from 'node-fetch'
 import { Swagger } from '../types'
+import * as fs from 'fs-extra'
 import ora from 'ora'
 import chalk from 'chalk'
+const isHttpReg = /^https?/
 /**
  * @description fetch the api json from remote
  * @param { url} swagger api url
  * @returns
  */
 export async function fetchApiJson(url: string): Promise<Swagger> {
-  let spinner = ora(`Fetch swagger api json from ${chalk.blue.underline(url)}`).start()
+  let spinner
   try {
+    if (!isHttpReg.test(url)) {
+      const res = await fs.readJson(url)
+      return res
+    }
+    spinner = ora(`Fetch swagger api json from ${chalk.blue.underline(url)}`).start()
     const res = await fetch(url, {}).then(res => res.json())
     spinner.succeed()
     return res
   } catch(err) {
-    spinner.fail(err.toString())
+    spinner && spinner.fail(err.toString())
   }
 }
 
