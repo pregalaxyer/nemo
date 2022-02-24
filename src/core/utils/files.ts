33bc1043ee7c7c7bf2ab939cbec05875dbb399f7
zjotaro <zzc5464@foxmail.com>
2022-02-24 10:14:44 +0800

    feat: 去掉.ts后缀

--- src/core/utils/files.ts
+++ src/core/utils/files.ts
@@ -1,21 +1,23 @@
-import * as fs from 'fs-extra'
-import * as path from 'path'
-import * as mustache from 'mustache'
-import chalk from 'chalk'
-const log = console.log
-export async function writeMustacheFile(temp: string, view: {
-  name: string
-  [key: string]: any
-}, src: string, templateMap?: Record<string, string> ) {
+import * as fs from "fs-extra";
+import * as path from "path";
+import * as mustache from "mustache";
+import chalk from "chalk";
+const log = console.log;
+export async function writeMustacheFile(
+  temp: string,
+  view: {
+    name: string;
+    [key: string]: any;
+  },
+  src: string,
+  templateMap?: Record<string, string>
+) {
   try {
-    const modelData = mustache.render(temp, view, templateMap || {})
-    const file = path.join(src, `./${view.name}.ts`)
-    await fs.writeFileSync(
-      file,
-      modelData
-    )
-    log(`🐠 nemo log: ` + chalk.yellow.underline(file) + ' created ✨')
-  } catch(err) {
-    throw Error(`write file error: ${err}`)
+    const modelData = mustache.render(temp, view, templateMap || {});
+    const file = path.join(src, `./${view.name}`);
+    await fs.writeFileSync(file, modelData);
+    log(`🐠 nemo log: ` + chalk.yellow.underline(file) + " created ✨");
+  } catch (err) {
+    throw Error(`write file error: ${err}`);
   }
 }
