6c36fe40cfafd9217f8a9c00cb211cdea37a1059
zjotaro <zzc5464@foxmail.com>
2022-02-24 10:30:59 +0800

    feat: 过滤.ts文件

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,25 +1,22 @@
 // TODO for lib test
 
-import main from '../esm/index'
-import * as fs from 'fs-extra'
-import path from 'path'
+import main from "../esm/index";
+import * as fs from "fs-extra";
+import path from "path";
 
-test('lib excute', async () => {
+test("lib excute", async () => {
   await main({
-    url: 'https://petstore.swagger.io/v2/swagger.json',
-    output: './tests/.output',
-    requestPath: null,
+    url: "https://petstore.swagger.io/v2/swagger.json",
+    output: "./tests/.output",
+    requestPath: "@/request.ts",
     paths: undefined,
-    exportsRequest: true
-  })
+    exportsRequest: true,
+  });
 
   const [models, services] = await Promise.all([
-    fs.readdirSync(path.resolve(__dirname, '.output/models')),
-    fs.readdirSync(path.resolve(__dirname,'.output/services'))
-  ])
-  expect(models).toBeDefined()
-  expect(services).toBeDefined()
-
-})
-
-
+    fs.readdirSync(path.resolve(__dirname, ".output/models")),
+    fs.readdirSync(path.resolve(__dirname, ".output/services")),
+  ]);
+  expect(models).toBeDefined();
+  expect(services).toBeDefined();
+});
